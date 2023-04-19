cead6dc6346a3f77fead4e84182f43767208cacb
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2023-04-19 02:25:49 +0200

    c

--- Multiplicators algorithm.js
+++ Multiplicators algorithm.js
@@ -1,23 +1,27 @@
-// Find multiplicators of multiplication
+// Find multiplicators of multiplication with different complexity algorithms
 
-// Time complexity: O(n^2)
-
-function numberIsPrime(n) {
+// HELPERS
+function checkIfNumberIsPrime(n) {
    let factor = 2;
    while (n % factor != 0) factor++;
    return factor == n;
 }
 
-function func(n) {
-   let n1 = 1,
-      n2 = 1,
-      results = [];
-
-   if (numberIsPrime(n))
+function ifNumberIsPrimeReturn(n) {
+   if (checkIfNumberIsPrime(n))
       return [
          { factor_1: 1, factor_2: n },
          { factor_1: n, factor_2: 1 },
       ];
+}
+
+// Time complexity func1: O(n^2)
+function func1(n) {
+   ifNumberIsPrimeReturn(n);
+
+   let n1 = 1,
+      n2 = 1,
+      results = [];
 
    for (let i = 1; i <= n; i++) {
       n1 = i;
@@ -33,13 +37,77 @@ function func(n) {
    return results;
 }
 
-console.time('Computational time');
-console.log('79777:', func(79777)); // number is prime
-console.timeEnd('Computational time');
+// Time complexity func3: O(n)
+function func2(n) {
+   ifNumberIsPrimeReturn(n);
 
-console.log('\n');
+   let i = 1,
+      results = [],
+      j = n;
+
+   while (i <= j) {
+      if (n % i === 0) results.push({ factor_1: i, factor_2: n / i });
+      i++;
+
+      if (n % j === 0 && j !== i && j !== n / i)
+         results.push({ factor_1: j, factor_2: n / j });
+
+      j--;
+   }
+
+   return results;
+}
+
+// Time complexity func3: O(sqrt(n))
+function func3(n) {
+   ifNumberIsPrimeReturn(n);
+
+   let results = [];
+
+   for (let i = 1; i <= Math.floor(Math.sqrt(n)); i++)
+      if (n % i === 0) results.push({ factor_1: i, factor_2: n / i });
+
+   if (Math.floor(Math.sqrt(n)) ** 2 === n) results.pop();
+
+   return results;
+}
+
+// TEST --------------------------------------------------------------------------
+
+let numPrime = 79777,
+   numNotPrime = 73_958_673_564_259_834;
+
+// // // Number is prime
+// console.time('Computational time');
+// console.log(`FUNC-1:\n ${numPrime}: `, func1(numPrime));
+// console.timeEnd('Computational time');
+
+console.log(
+   '----------------------------------------------------------------\n'
+);
+
+// // // FUNC 1: O(n^2) ------- Cazz se ce ne mette
+
+// console.time('Computational time');
+// console.log(`FUNC-1:\n ${numNotPrime}: `, func1(numNotPrime));
+// console.timeEnd('Computational time');
+
+console.log(
+   '----------------------------------------------------------------\n'
+);
+
+// // // FUNC 2: O(n)
+
+// console.time('Computational time');
+// console.log(`FUNC-2:\n ${numNotPrime}: `, func2(numNotPrime));
+// console.timeEnd('Computational time');
+
+console.log(
+   '----------------------------------------------------------------\n'
+);
+
+// // // FUNC 3: O(sqrt(n))
 
 console.time('Computational time');
-let n = 797777
-console.log(`${n}: `, func(n));
+console.log(`FUNC-3:\n ${numNotPrime}: `, func3(numNotPrime));
 console.timeEnd('Computational time');
